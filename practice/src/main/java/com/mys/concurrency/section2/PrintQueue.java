e76868e82d2071dc880c6f9f6c7c4a5cce2fb9b6
manyansong <manyansong@jk.cn>
2016-10-30 12:18:13 +0800

    锁的公平性

--- practice/src/main/java/com/mys/concurrency/section2/PrintQueue.java
+++ practice/src/main/java/com/mys/concurrency/section2/PrintQueue.java
@@ -8,11 +8,17 @@ import java.util.concurrent.locks.ReentrantLock;
  * Created by ASUS on 2016/10/27.
  */
 public class PrintQueue {
-    private final Lock lock = new ReentrantLock();//声明可重入锁
+//    private final Lock lock = new ReentrantLock();//声明可重入锁
+    private final Lock lock = new ReentrantLock(); //参数默认false，不公平锁
+//    private final Lock lock = new ReentrantLock(true); //公平锁
     public void printJob(Object document){
 //        Lock lock = new ReentrantLock();
         lock.lock();//获取对锁对象的控制
 
+        printJob(lock);
+    }
+
+    private static void printJob(Lock lock) {
         try {
             Long duration = (long)(Math.random()*10000);
             System.out.println(Thread.currentThread().getName()+":PrintQueue:Printing a Job during "+
@@ -28,16 +34,7 @@ public class PrintQueue {
     public void printJob2(Object document){
 //        ReentrantLock queueLock = new ReentrantLock();
         if (lock.tryLock()) {  //如果已经被lock，则立即返回false不会等待，达到忽略操作的效果
-            try {
-                Long duration = (long)(Math.random()*10000);
-                System.out.println(Thread.currentThread().getName()+":PrintQueue:Printing a Job during "+
-                        (duration/1000)+" seconds");
-                Thread.sleep(duration);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();//一定要释放，否则会死锁
-            }
+            printJob(lock);
         }else {
             //如果lock是局部变量（每个线程是一把锁因此不会有公共的资源需要抢占），就不会走到else语句体
             System.out.println(Thread.currentThread().getName()+"没有获取到锁，生气不打印啦。。。");
@@ -47,22 +44,13 @@ public class PrintQueue {
 
     public void printJob3(Object document){
 
-        //公平情况下，操作会排一个队按顺序执行，来保证执行顺序。
+        //公平情况下，操作会排一个队按顺序执行（等待时间最长的优先），来保证执行顺序。
         //不公平情况下，是无序状态允许插队，jvm会自动计算如何处理更快速来调度插队，性能更高。
         ReentrantLock lock = new ReentrantLock(); //参数默认false，不公平锁
 //        ReentrantLock lock = new ReentrantLock(true); //公平锁
 
         lock.lock(); //如果被其它资源锁定，会在此等待锁释放，达到暂停的效果
-        try {
-            Long duration = (long)(Math.random()*10000);
-            System.out.println(Thread.currentThread().getName()+":PrintQueue:Printing a Job during "+
-                    (duration/1000)+" seconds");
-            Thread.sleep(duration);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
+        printJob(lock);
     }
 
     public void printJob4(Object document){
@@ -97,7 +85,7 @@ public class PrintQueue {
         //公平情况下，操作会排一个队按顺序执行，来保证执行顺序。
         //不公平情况下，是无序状态允许插队，jvm会自动计算如何处理更快速来调度插队，性能更高。
 //        ReentrantLock lock = new ReentrantLock(); //参数默认false，不公平锁
-        ReentrantLock lock = new ReentrantLock(true); //公平锁
+//        ReentrantLock lock = new ReentrantLock(true); //公平锁
 
         try {
             lock.lockInterruptibly();
